e3deaf10e453fd5c8581eee4591a61c3d1e026eb
Gleb <gleb-vinnikov@mail.ru>
2018-02-20 08:25:17 +0300

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -13,7 +13,19 @@ class SelfOrganizedList {
     }
 
     insert(data) {
-
+        var node = new Node(data);
+        this.tail = node;
+        if (!this.head) {
+            this.head = node;
+        } else {
+            var a = this.head;
+            while (a.next) {
+                a = a.next;
+            }
+            a.next = node;
+            a.next.prev = a;
+            this.tail.prev = a;
+        }
     }
 
     size() {
